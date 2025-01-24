8414d6f8f425b0db7af56dd07b6fb12499adc2ee
Matthias Huber <mhuber@churchtools.de>
2025-01-24 22:26:23 +0100

    add UpdateComponentColor in init

--- BubbleTrubble/Assets/Scripts/Bubble.cs
+++ BubbleTrubble/Assets/Scripts/Bubble.cs
@@ -8,6 +8,7 @@ public class Bubble : MonoBehaviour
     void Start()
     {
         bubbleColor = BubbleColor.White;
+        UpdateComponentColor();
     }
 
     // Update is called once per frame
