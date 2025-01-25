3dfa15201f022ad08cbbd7e5ec6414909a54e764
Elias Schaut <eschaut@outlook.de>
2025-01-25 17:56:33 +0100

    hotfix

--- BubbleTrubble/Assets/Scripts/Bubble.cs
+++ BubbleTrubble/Assets/Scripts/Bubble.cs
@@ -31,11 +31,6 @@ public class Bubble : MonoBehaviour, IInteractable
         soundPlayer = GetComponent<MultiAudioSourcePlayer>();
         BubbleColors.SetObjectColor(gameObject, bubbleColor);
     }
-
-    void Update()
-    {
-        
-    }
     
     public void SetColor(BubbleColor color)
     {
@@ -123,6 +118,6 @@ public class Bubble : MonoBehaviour, IInteractable
 
     void OnCollisionEnter(GameObject bubble)
     {
-        
+        throw new System.NotImplementedException();
     }
 }
