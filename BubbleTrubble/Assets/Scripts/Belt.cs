b920e93ebc62c26b838ee26067c1333bd6ba5fd4
Elias Schaut <eschaut@outlook.de>
2025-01-25 13:33:53 +0100

    update

--- BubbleTrubble/Assets/Scripts/Belt.cs
+++ BubbleTrubble/Assets/Scripts/Belt.cs
@@ -1,17 +1,13 @@
 using UnityEngine;
 
+public enum BeltType
+{
+    Straight,
+    LeftTurn,
+    RightTurn,
+}
+
 public class Belt : MonoBehaviour
 {
-    void Update()
-    {
-        IBelt self = this;
-        self.BeldUpdate();
-    }
-
-    bool IBelt.IsOccupied { get; set; } = false;
-    IBelt IBelt.NextBelt { get; set; }
-    GameObject IBelt.Bubble { get; set; }
-    float IBelt.MovementSpeed { get; set; } = 1.0f;
-    Vector2 IBelt.MovementDirection { get; set; }
-    Vector2 IBelt.Position => transform.position;
+    public BeltType beltType { get; set; } = BeltType.Straight;
 }
