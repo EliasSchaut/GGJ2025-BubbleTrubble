6258820970be817daa8fa3ea8fcd351d653d6b90
Jean Patrick Mathes <jean.mathes@student.kit.edu>
2025-01-25 16:37:45 +0100

    Advanced enemy stuff

---
+++ BubbleTrubble/Assets/Scripts/EnemyMothership.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class EnemyMothership : MonoBehaviour
+{
+    [SerializeField] private GameObject[] enemies;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private float interArrivalTime = 3f;
+    
+    private float timer;
+    
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer < interArrivalTime) return;
+
+        timer = 0;
+            
+        int index = Random.Range(0, enemies.Length);
+        Instantiate(enemies[index], spawnPoint.position, Quaternion.identity);
+    }
+}
