a82817a5b2231e87cd49bb97904e63c66d4ebcfb
Matthias Huber <mhuber@churchtools.de>
2025-01-25 20:52:33 +0100

    fix detaching of Bubble from player

--- BubbleTrubble/Assets/Scripts/Belt.cs
+++ BubbleTrubble/Assets/Scripts/Belt.cs
@@ -20,6 +20,7 @@ public class Belt : MonoBehaviour, IInteractable
         Debug.Log("interacted with belt " + gridPosition);
         if (!player.HoldsBubble()) return false;
         GameObject bubble = player.GetBubble();
+        bubble.transform.parent = transform;
         Bubble bubbleComponent = bubble.GetComponent<Bubble>();
         player.SetBubble(null);
         if (beltGrid.DestroyIfCollidingBubbles(bubbleComponent)) return true;
