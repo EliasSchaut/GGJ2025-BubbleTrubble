619f060b297d2720d611bd21d491f5bd7fa220c4
Matthias Huber <mhuber@churchtools.de>
2025-01-26 15:12:31 +0100

    add wave handling for background music

--- BubbleTrubble/Assets/Scripts/WaveManager.cs
+++ BubbleTrubble/Assets/Scripts/WaveManager.cs
@@ -7,6 +7,8 @@ public class WaveManager : MonoBehaviour
 
     [SerializeField] private Transform spawnA;
     [SerializeField] private Transform spawnB;
+
+    [SerializeField] private GameObject soundManager;
     
     [SerializeField] private float waveDuration = 5.0f;
     
@@ -24,37 +26,37 @@ public class WaveManager : MonoBehaviour
         
         min = Vector3.Min(spawnA.position, spawnB.position);
         max = Vector3.Max(spawnA.position, spawnB.position);
-        
-        StartWave();
+
+        timer = 15.0f;
     }
-    
+
     private void StartWave()
     {
         currentWave += 1;
         GameManager.Instance.SetWave(currentWave);
         timer = 0.0f;
-        
+
         (int childships, int mothership) counts = GetShipsCount();
-        
-        for (int i = 0; i < counts.childships; i++)
-        {
+
+        for (int i = 0; i < counts.childships; i++) {
             Vector3 spawnPosition = GetSpawnPosition();
             Instantiate(GetElement(childships), spawnPosition, gameObject.transform.rotation);
         }
 
-        if (counts.mothership > 0)
-        {
+        if (counts.mothership > 0) {
             currentMothershipWave += 1;
         }
-        
-        for (int i = 0; i < counts.mothership; i++)
-        {
+
+        for (int i = 0; i < counts.mothership; i++) {
             Vector3 spawnPosition = GetSpawnPosition();
-            
-            EnemyMothership mothership = Instantiate(GetElement(motherships), spawnPosition, Quaternion.identity).GetComponent<EnemyMothership>();
-            
+
+            EnemyMothership mothership = Instantiate(GetElement(motherships), spawnPosition, Quaternion.identity)
+                .GetComponent<EnemyMothership>();
+
             mothership.Wave = currentMothershipWave;
         }
+
+        soundManager.GetComponent<SoundManager>().SwitchToIntense();
     }
 
     private void Update()
